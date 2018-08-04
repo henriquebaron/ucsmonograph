66be5163142189dc8b6a5692d7c61fc7078e0d79
Henrique Baron <henrique.baron@gmail.com>
2018-08-04 11:44:44 -0300

    Batch de compilação agora inclui o arquivo DTX e compacta a pasta.

--- Compilar TDS.bat
+++ Compilar TDS.bat
@@ -1,8 +1,12 @@
 @echo off
 del ucsmono.cls /Q
+del ucsmono.tds.zip /Q
 latex ucsmono.ins
 pdflatex ucsmono.dtx
 mkdir ucsmono.tds\tex\latex\ucsmono
 mkdir ucsmono.tds\doc\latex\ucsmono
+mkdir ucsmono.tds\source\latex\ucsmono
 xcopy ucsmono.cls ucsmono.tds\tex\latex\ucsmono /K /I /Y
 xcopy ucsmono.pdf ucsmono.tds\doc\latex\ucsmono /K /I /Y
+xcopy ucsmono.dtx ucsmono.tds\source\latex\ucsmono /K /I /Y
+"C:\Program Files\7-Zip\7z" a ucsmono.tds.zip .\ucsmono.tds\*
