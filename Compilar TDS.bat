d0935918b7c5f47d5a374ac1e1c7dc2eb7b99351
Henrique Baron <henrique.baron@gmail.com>
2018-08-04 16:48:19 -0300

    Arquivo .INS também é copiado para a pasta source.

--- Compilar TDS.bat
+++ Compilar TDS.bat
@@ -9,4 +9,5 @@ mkdir ucsmono.tds\source\latex\ucsmono
 xcopy ucsmono.cls ucsmono.tds\tex\latex\ucsmono /K /I /Y
 xcopy ucsmono.pdf ucsmono.tds\doc\latex\ucsmono /K /I /Y
 xcopy ucsmono.dtx ucsmono.tds\source\latex\ucsmono /K /I /Y
+xcopy ucsmono.ins ucsmono.tds\source\latex\ucsmono /K /I /Y
 "C:\Program Files\7-Zip\7z" a ucsmono.tds.zip .\ucsmono.tds\*
