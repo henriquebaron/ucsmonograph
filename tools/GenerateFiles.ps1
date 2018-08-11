adf699496f4b7610cffbfc137e2a2fe9e4096011
Henrique Baron <henrique.baron@gmail.com>
2018-08-11 08:47:20 -0300

    Resolução da issue \#4 - Criados scripts do PowerShell para compilar os arquivos e fazer a distribuição.

--- tools/GenerateFiles.ps1
+++ tools/GenerateFiles.ps1
@@ -1,8 +1,14 @@
 $class = "ucsmonograph"
-del *.cls,*.pdf
+
+# Apaga a compilação anterior
+Remove-Item *.cls,*.pdf
+
+# Compila os documentos da classe e da documentação
 latex ./$class.ins
 pdflatex ./$class.dtx
 makeindex -s gglo.ist -o ./$class.gls ./$class.glo
 makeindex -s gind.ist -o ./$class.ind ./$class.idx
 pdflatex ./$class.dtx
-del *.aux,*.glo,*.gls,*.gls,*.idx,*.ilg,*.ind,*.log
+
+# Apaga os arquivos auxiliares
+Remove-Item *.aux,*.glo,*.gls,*.gls,*.idx,*.ilg,*.ind,*.log
